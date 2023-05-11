50d6a29b3c139851fba3ad3a70e28ce15e54f825
response <platoon1277@gmail.com>
2023-05-11 18:50:22 +0100

    The routing and icons were implimented

--- e-commerceapp/src/App.js
+++ e-commerceapp/src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
-// import { Routes, Route } from "react-router-dom";
-// import SignUpPage from "./Components/FirstPage/SignUpPage";
-// import LoginPage from "./Components/FirstPage/LoginPage";
-// import ForgetPasswordPage from "./Components/FirstPage/ForgetPasswordPage";
+import { Routes, Route } from "react-router-dom";
+import SignUpPage from "./Components/FirstPage/SignUpPage";
+import LoginPage from "./Components/FirstPage/LoginPage";
+import ForgetPasswordPage from "./Components/FirstPage/ForgetPasswordPage";
 
 function App() {
   return (
-    <p>Hello world!</p>
-    // <Routes>
-    //   <Route index element={<SignUpPage />} />
-    //   <Route path="Login" element={<LoginPage />} />
-    //   <Route path="ForgetPasswordPage" element={<ForgetPasswordPage />} />
-    // </Routes>
+    
+    <Routes>
+      <Route index element={<SignUpPage />} />
+      <Route path="Login" element={<LoginPage />} />
+      <Route path="ForgetPasswordPage" element={<ForgetPasswordPage />} />
+    </Routes>
   );
 }
 
