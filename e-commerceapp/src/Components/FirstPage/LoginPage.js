e487347a15547091579a20485e941ad699834b12
response <platoon1277@gmail.com>
2023-05-12 08:42:11 +0100

    the web view of the first page was updated

--- e-commerceapp/src/Components/FirstPage/LoginPage.js
+++ e-commerceapp/src/Components/FirstPage/LoginPage.js
@@ -5,6 +5,7 @@ import Button from "../Button/button";
 import FirstName from "../InputTags/First-name/FirstName";
 import Password from "../InputTags/Password/Password";
 import HeaderPage from "./HeaderPage/HeaderPage";
+// All icons, icon-flex, links are syled in the app.css file
 
 function LoginPage() {
   return (
@@ -14,15 +15,18 @@ function LoginPage() {
       <Password />
       <p className="link1">
         {" "}
-        forgot You Password? <Link to="/ForgetPasswordPage">
+        forgot You Password?{" "}
+        <Link to="/ForgetPasswordPage">
           <FaArrowRight className="icon icon1" />
         </Link>
       </p>
       <Button props="Login" />
       <br />
       <p className="link2">Or Login with our Social media Accounts</p>
-      <FaGoogle className="icon icon2" /> <FaFacebook className="icon icon3" />
-      <Outlet />
+      <div className="icon-flex">
+        <FaGoogle className="icon icon2" />{" "}
+        <FaFacebook className="icon icon3" />
+      </div>
     </>
   );
 }
