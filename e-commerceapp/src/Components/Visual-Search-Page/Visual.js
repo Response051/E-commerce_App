3571c2a22955dd81d40a3b929650be23eeb57f40
response <platoon1277@gmail.com>
2023-05-12 09:05:35 +0100

    A test commit using the gitExtention in the vscode

---
+++ e-commerceapp/src/Components/Visual-Search-Page/Visual.js
@@ -0,0 +1,17 @@
+import React from "react";
+import HeaderPage from "../FirstPage/HeaderPage/HeaderPage";
+import Button from "../Button/button";
+
+function Visual() {
+  return (
+    <>
+      <HeaderPage props=" Visual" />
+      <div>
+        <p>Search for an outfit by taking a photo or uploading an image</p>
+        <Button prop="" />
+      </div>
+    </>
+  );
+}
+
+export default Visual;
